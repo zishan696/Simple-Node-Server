1870c04ee87a548d29d11327393b52afb6f60aaa
Ahshan Zishan <dantex696@outlook.com>
2018-07-09 04:27:29 +0000

    classwork5

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
+var fs=require('fs');
   var server = http.createServer(function(req, res){
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', function(err,data){
+        if(err){
+            return console.log("file read error");
+        }
+        res.end(data);
+    });
   });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
