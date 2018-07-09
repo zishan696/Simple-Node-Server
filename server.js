7aa3953f21de2f3e865b063c0767b240ada7e96f
Ahshan Zishan <dantex696@outlook.com>
2018-07-09 04:07:14 +0000

    classwork5

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+  var server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello World\n");
+  });
+  server.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server running');
+  });
