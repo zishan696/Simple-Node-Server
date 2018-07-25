133965a2dc01df67db11c89183c9f965f4411be7
Ahshan Zishan <dantex696@outlook.com>
2018-07-25 05:04:19 +0000

    Class Work 8

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+
 /*var fs=require('fs');
   var server = http.createServer(function(req, res){
     res.statusCode = 200;
@@ -11,24 +11,61 @@ var http = require('http');
     });
   });*/
   
-  var express=require('express');
+var http = require('http');
+//var mongoose = require ('mongoose');
+var mongo=require('mongodb');
+  var express = require('express');
   var app = express();
-  var server = http.Server(app);
-  var bodyParser= require('body-parser');
+  var bodyParser = require('body-parser');
+ 
+  
+var db, uri = "mongodb://" + process.env.IP + "/test";
+   
+  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+    if(err){console.log("Could not connect to MongoDB")
+    }
+    else {
+      db = client.db('simplenode');
+      console.log("Database Created")
+    }
+  });
+   
+  var save = function(form_data){
+    db.createCollection('users',function(err,collection){ if (err) throw err;});
+    var collection = db.collection('users');
+    collection.save(form_data);
+    
+  }
+  
+ var server = http.Server(app);
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
   
-  app.get('/', function(req,res){
+  app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
+  })
+  
+  app.get('/about', function(req, res){
+    res.sendFile(__dirname+'/about.html');
   });
-  app.get('/system/form', function(req,res){
+  
+   app.get('/form', function(req, res){
     res.sendFile(__dirname+'/form.html');
   });
   
-  app.post('/submit_user', function(req,res){
-    console.log(req.body);
-  });
-  
+  app.post('/signup', function(req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    console.log("post received: %s %s", username, email);
+});
+
+app.post('/submit_user',function(req, res){
+  console.log(req.body);
+   save(req.body);
+  res.status('200');
+});
+
+ 
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
-  });
+});
