2d34c4657c97d7b4349f54d029c93749300cce93
Ahshan Zishan <dantex696@outlook.com>
2018-07-30 04:36:12 +0000

    Class Work 9

--- server.js
+++ server.js
@@ -12,23 +12,36 @@
   });*/
   
 var http = require('http');
-//var mongoose = require ('mongoose');
-var mongo=require('mongodb');
+
+var mongoose=require('mongoose');
   var express = require('express');
   var app = express();
   var bodyParser = require('body-parser');
- 
   
-var db, uri = "mongodb://" + process.env.IP + "/test";
-   
-  mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
-    if(err){console.log("Could not connect to MongoDB")
-    }
-    else {
-      db = client.db('simplenode');
-      console.log("Database Created")
-    }
+  mongoose.connect("mongodb://localhost:27017/node-cw8");
+  mongoose.connection.on('error', function(){
+    console.log('caould not connect to mongodb');
+  })
+  
+  var userSchema = new mongoose.Schema({
+    name:{
+      type:String,
+      required:"Name is required"
+    },
+    email:String
   });
+ var User = mongoose.model('User', userSchema);
+ //var mongoose = require ('mongoose');
+// var db, uri = "mongodb://" + process.env.IP + "/test";
+   
+//   mongo.MongoClient.connect(uri,{userNewUrlParser:true},function(err,client){
+//     if(err){console.log("Could not connect to MongoDB")
+//     }
+//     else {
+//       db = client.db('simplenode');
+//       console.log("Database Created")
+//     }
+//   });
    
   var save = function(form_data){
     db.createCollection('users',function(err,collection){ if (err) throw err;});
@@ -61,8 +74,16 @@ var db, uri = "mongodb://" + process.env.IP + "/test";
 
 app.post('/submit_user',function(req, res){
   console.log(req.body);
-   save(req.body);
-  res.status('200');
+  var new_user=new User(req.body);
+  new_user.save(function(err,data){
+    if(err){
+      return res.status(400)
+      .json({message:"Couldn't save user"})
+    }
+    res.status(200).json(data);
+  })
+  // save(req.body);
+  // res.status('200');
 });
 
  
